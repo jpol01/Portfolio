b119ac55e44a1b38ea17c8f3669fdd9eea818358
Jorge Polo <39609857+jpol01@users.noreply.github.com>
2022-01-24 21:04:21 -0500

    Update javascript.js
    erasing extra line

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ function closeNav() {
   document.querySelector('#myNav').style.height = '0%';
 }
 
-
 document.getElementById('open-menu').onclick = openNav;
 document.getElementById('close-nav').onclick = closeNav;
 document.getElementById('close-nav1').onclick = closeNav;
