dc22e68a0d8221ab4d78aa111c5c78df3c8aaa2f
Carlos Salinas <jpolo.santamaria@gmail.com>
2022-01-25 09:46:42 -0500

    reomoved xtra js

--- javascript.js
+++ javascript.js
@@ -6,8 +6,3 @@ function closeNav() {
   document.querySelector('#myNav').style.height = '0%';
 }
 
-document.getElementById('open-menu').onclick = openNav;
-document.getElementById('close-nav').onclick = closeNav;
-document.getElementById('close-nav1').onclick = closeNav;
-document.getElementById('close-nav2').onclick = closeNav;
-document.getElementById('close-nav3').onclick = closeNav;
